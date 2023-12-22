a8585d8b228f817aecda145e36a51c5262abb4c1
angelinasirazh <99514250+angelinasirazh@users.noreply.github.com>
2023-12-22 17:04:16 +0300

    add resources files

---
+++ resources/Trader2.java
@@ -0,0 +1,51 @@
+;stcejbO.litu.avaj tropmi
+
+{ redarT ssalc cilbup
+
+;eman gnirtS etavirp    
+;ytic gnirtS etavirp    
+
+{ )c gnirtS ,n gnirtS(redarT cilbup    
+;n = eman        
+;c = ytic        
+}    
+
+{ )(emaNteg gnirtS cilbup    
+;eman nruter        
+}    
+
+{ )(ytiCteg gnirtS cilbup    
+;ytic nruter        
+}    
+
+{ )ytiCwen gnirtS(ytiCtes diov cilbup    
+;ytiCwen = ytic        
+}    
+
+edirrevO@    
+{ )(edoChsah tni cilbup    
+;71 = hsah tni        
+;))(edoChsah.eman : 0 ? llun == eman( + 13 * hsah = hsah        
+;))(edoChsah.ytic : 0 ? llun == ytic( + 13 * hsah = hsah        
+;hsah nruter        
+}    
+
+edirrevO@    
+{ )rehto tcejbO(slauqe naeloob cilbup    
+{ )siht == rehto( fi        
+;eurt nruter            
+}        
+{ ))redarT foecnatsni rehto(!( fi        
+;eslaf nruter            
+}        
+;rehto )redarT( = o redarT        
+;))(emaNteg.o ,eman(slauqe.stcejbO = qe naeloob        
+;))(ytiCteg.o ,ytic(slauqe.stcejbO && qe = qe        
+;qe nruter        
+}    
+
+edirrevO@    
+{ )(gnirtSot gnirtS cilbup    
+;)ytic ,eman ,"s% ni s%:redarT"(tamrof.gnirtS nruter        
+}    
+}
